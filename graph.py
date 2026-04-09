7ca336e2fcee4fe98881497aaf78490141eaa669
hipstermartin <aamilabdulla0@gmail.com>
2026-04-09 18:03:51 +0400

    completed graph.py

--- graph.py
+++ graph.py
@@ -1,7 +1,6 @@
 from langgraph.graph import StateGraph , END
 from typing import TypedDict
 import os
-from langchain_groq import chat_qroq
 from groq import Groq
 from  e2b_code_interpreter import Sandbox 
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
@@ -70,23 +69,32 @@ def execute_node(state: AgentState):
             return{
                 "success": False,
                 "explanation": f"Error: {execution.error}",
-                "attempts": state["attempts_count"] + [{"code": state["current_code"], "error": execution.error}]
+                "attempts": state["attempts"] + [{"code": state["current_code"], "error": execution.error}]
             }
             
         else:
             return{
                 "success": True,
                 "explanation": f"Output: {execution.logs.stdout}",
-                "attempts": state["attempts_count"] + [{"code": state["current_code"], "output": execution.logs.stdout}]
-            }
-
-
-    
-    return {"current_code": state["current_code"]}  
+                "attempts": state["attempts"] + [{"code": state["current_code"], "output": execution.logs.stdout}]
+            }  
 
 def explain_node(state: AgentState):
     print("Explaining the results")
-    return {"explanation": state["explanation"]}
+    completion = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[
+            {
+                "role": "system",
+                "content": f"You are a pro code explainer assistance that explains the execution results of the code using the task description which user has given . Return ONLY the explanation. Do not include markdown code blocks, backticks, or any conversational text."
+                },
+                {
+                "role": "user",
+                "content": f" the original code : {state['original_code']} , the fixed code : {state['current_code']} and the analysis : {state['analysis']} and the attempts : {state['attempts']} ."
+                }
+        ]
+        )
+    return {"explanation": completion.choices[0].message.content}
 
 def route_after_execute(state: AgentState):
     if state["success"]:
@@ -105,5 +113,7 @@ graph.add_node("explain" , explain_node)
 graph.set_entry_point("analyzer")
 graph.add_edge("analyzer", "fix")
 graph.add_edge("fix", "execute")
-graph.add_conditional_edge("execute", route_after_execute)
+graph.add_conditional_edges("execute", route_after_execute)
 graph.add_edge("explain", END)
+
+app = graph.compile()
